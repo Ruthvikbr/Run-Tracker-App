26610d947519ecef70cef96aa18cf5ba569a5bfc
Ruthvikbr <ruthvikbr24@gmail.com>
2020-09-20 21:16:57 +0530

    handle map lifecycle

--- app/src/main/java/com/ruthvikbr/runtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/ruthvikbr/runtracker/ui/fragments/RunFragment.kt
@@ -23,7 +23,7 @@ class RunFragment : Fragment(R.layout.fragment_run) , EasyPermissions.Permission
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        requestPermissions()
         fab.setOnClickListener{
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
