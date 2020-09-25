cc641e48a813cdee0273bc437ee544f0b4b63036
Ruthvikbr <ruthvikbr24@gmail.com>
2020-09-25 16:37:03 +0530

    setup statistics fragment

--- app/src/main/java/com/ruthvikbr/runtracker/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/ruthvikbr/runtracker/ui/fragments/StatisticsFragment.kt
@@ -1,12 +1,55 @@
 package com.ruthvikbr.runtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ruthvikbr.runtracker.R
 import com.ruthvikbr.runtracker.ui.viewmodels.StatisticsViewModel
+import com.ruthvikbr.runtracker.utilities.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_statistics.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics){
     private val viewModel: StatisticsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers(){
+        viewModel.totalTimeRun.observe(viewLifecycleOwner,{
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                tvTotalTime.text = totalTimeRun
+            }
+        })
+
+        viewModel.totalDistance.observe(viewLifecycleOwner,{
+            it?.let {
+                val km = it/1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}kms"
+                tvTotalDistance.text = totalDistanceString
+            }
+        })
+
+        viewModel.averageSpeed.observe(viewLifecycleOwner,{
+            it?.let {
+               val avgSpeed = round(it * 10f) / 10f
+                val averageSpeedString = "${avgSpeed}km/h"
+                tvAverageSpeed.text = averageSpeedString
+            }
+        })
+
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner,{
+            it?.let {
+                val totalCalories = "${it}kcal"
+                tvTotalCalories.text = totalCalories
+            }
+        })
+    }
 }
--- app/src/main/java/com/ruthvikbr/runtracker/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/ruthvikbr/runtracker/ui/viewmodels/StatisticsViewModel.kt
@@ -4,7 +4,16 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.ruthvikbr.runtracker.repositories.MainRepository
 
+
 class StatisticsViewModel @ViewModelInject constructor(
-    val mainRepository: MainRepository
+    private val mainRepository: MainRepository
 ) : ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurnt()
+    val averageSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
 }
